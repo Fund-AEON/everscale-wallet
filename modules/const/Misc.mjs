6b4f8f9ff68402bb45d6cea4b75faf776e0dc2a1
Andrey Nedobylsky <admin@twister-vl.ru>
2022-04-08 14:22:54 +0200

    Everscale Wallet update

--- modules/const/Misc.mjs
+++ modules/const/Misc.mjs
@@ -1,25 +1,17 @@
-/*
-  _____ ___  _   ___        __    _ _      _
- |_   _/ _ \| \ | \ \      / /_ _| | | ___| |_
-   | || | | |  \| |\ \ /\ / / _` | | |/ _ \ __|
-   | || |_| | |\  | \ V  V / (_| | | |  __/ |_
-   |_| \___/|_| \_|  \_/\_/ \__,_|_|_|\___|\__|
-
- */
 /**
- * @name FreeTON browser wallet and injector
- * @copyright SVOI.dev Labs - https://svoi.dev
+ * @name Everscale Wallet browser wallet
+ * @copyright Fund AEON
  * @license Apache-2.0
  * @version 1.0
  */
 
 
 const MISC = {
-    VERSION: '0.0.11',
+    VERSION: '1.0.0',
     POPUP_FOCUSOUT_CLOSE_TIMER: 120000,
     POPUP_PARAMS:{
-        width: 350,
-        height: 600,
+        width: 400,
+        height: 650,
     }
 };
 
